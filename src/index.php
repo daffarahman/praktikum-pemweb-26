ea03ced5296573e1b149cf6aadab2f395a489800
root <root@hazelnutserver.fatisda.dev>
2026-05-26 02:01:16 +0000

    prepare: tugas3 and 4

--- src/index.php
+++ src/index.php
@@ -1,43 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dap's Pemweb</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body>
-
-<h1>Welcome to Dap's Prak Pemweb Repo</h1>
-<hr>
-<p>This is basiscally my tugas wkwkwk</p>
-
-<div style="border: 1px solid black;padding: 0.5rem 2rem;">
-    <p>Muhammad Daffa Rahman</p>
-    <p>NIM L0124062</p>
-</div>
-
-<h2>Check it out here:</h2>
-
-<?php
-$path = __DIR__;
-$items = scandir($path);
-
-echo "<ul>";
-
-foreach ($items as $item) {
-    if ($item === '.' || $item === '..') continue;
-
-    if (is_dir($path . DIRECTORY_SEPARATOR . $item)) {
-        $url = htmlspecialchars($item) . '/';
-        $name = htmlspecialchars(ucwords($item));
-
-        echo "<li><a href=\"$url\">$name</a></li>";
-    }
-}
-
-echo "</ul>";
-
-?>
-    
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </body>
+
 </html>
